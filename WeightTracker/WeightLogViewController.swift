578639c8dafd8bf33d166f327312c21cc87a2e08
Cristian Rojas <cristian.rojas@live.fr>
2020-06-10 20:38:08 +0200

    viewcontrollers added

---
+++ WeightTracker/WeightLogViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeightLogViewController.swift
+//  WeightTracker
+//
+//  Created by Cristian Rojas on 10/06/2020.
+//  Copyright © 2020 Cristian Rojas. All rights reserved.
+//
+
+import UIKit
+
+class WeightLogViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
