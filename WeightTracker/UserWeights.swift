6936e463affbe4a3ed4ebcf96af4c95a06f1184b
Cristian Rojas <cristian.rojas@live.fr>
2020-06-10 20:51:48 +0200

    coredata class created

---
+++ WeightTracker/UserWeights.swift
@@ -0,0 +1,16 @@
+//
+//  UserWeights.swift
+//  WeightTracker
+//
+//  Created by Cristian Rojas on 10/06/2020.
+//  Copyright © 2020 Cristian Rojas. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class UserWeights: NSManagedObject {
+    @NSManaged var weight: String
+    @NSManaged var date: String
+    @NSManaged var units: String
+}
